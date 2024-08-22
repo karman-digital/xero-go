334a221bea8db5395f895f3f3026e32b4dd3ef34
Jaime Boardman <jaime@karman.digital>
2024-08-22 17:02:34 +0100

    add contact persons

--- models/contacts/body.go
+++ models/contacts/body.go
@@ -1,25 +1,26 @@
 package contacts
 
 type Contact struct {
-	ContactID                 string    `json:"ContactID,omitempty"`
-	ContactStatus             string    `json:"ContactStatus,omitempty"`
-	ContactNumber             string    `json:"ContactNumber,omitempty"`
-	AccountNumber             string    `json:"AccountNumber,omitempty"`
-	Name                      string    `json:"Name"`
-	FirstName                 string    `json:"FirstName,omitempty"`
-	LastName                  string    `json:"LastName,omitempty"`
-	CompanyNumber             string    `json:"CompanyNumber,omitempty"`
-	EmailAddress              string    `json:"EmailAddress,omitempty"`
-	BankAccountDetails        string    `json:"BankAccountDetails,omitempty"`
-	TaxNumber                 string    `json:"TaxNumber,omitempty"`
-	AccountsReceivableTaxType string    `json:"AccountsReceivableTaxType,omitempty"`
-	AccountsPayableTaxType    string    `json:"AccountsPayableTaxType,omitempty"`
-	Addresses                 []Address `json:"Addresses,omitempty"`
-	Phones                    []Phone   `json:"Phones,omitempty"`
-	UpdatedDateUTC            string    `json:"UpdatedDateUTC,omitempty"`
-	IsSupplier                bool      `json:"IsSupplier,omitempty"`
-	IsCustomer                bool      `json:"IsCustomer,omitempty"`
-	DefaultCurrency           string    `json:"DefaultCurrency,omitempty"`
+	ContactID                 string          `json:"ContactID,omitempty"`
+	ContactStatus             string          `json:"ContactStatus,omitempty"`
+	ContactNumber             string          `json:"ContactNumber,omitempty"`
+	AccountNumber             string          `json:"AccountNumber,omitempty"`
+	Name                      string          `json:"Name"`
+	FirstName                 string          `json:"FirstName,omitempty"`
+	LastName                  string          `json:"LastName,omitempty"`
+	CompanyNumber             string          `json:"CompanyNumber,omitempty"`
+	EmailAddress              string          `json:"EmailAddress,omitempty"`
+	BankAccountDetails        string          `json:"BankAccountDetails,omitempty"`
+	TaxNumber                 string          `json:"TaxNumber,omitempty"`
+	AccountsReceivableTaxType string          `json:"AccountsReceivableTaxType,omitempty"`
+	AccountsPayableTaxType    string          `json:"AccountsPayableTaxType,omitempty"`
+	Addresses                 []Address       `json:"Addresses,omitempty"`
+	Phones                    []Phone         `json:"Phones,omitempty"`
+	ContactPersons            []ContactPerson `json:"ContactPersons,omitempty"`
+	UpdatedDateUTC            string          `json:"UpdatedDateUTC,omitempty"`
+	IsSupplier                bool            `json:"IsSupplier,omitempty"`
+	IsCustomer                bool            `json:"IsCustomer,omitempty"`
+	DefaultCurrency           string          `json:"DefaultCurrency,omitempty"`
 }
 
 type Address struct {
@@ -39,4 +40,11 @@ type Phone struct {
 
 type Contacts struct {
 	Contacts []Contact `json:"Contacts"`
+}
+
+type ContactPerson struct {
+	FirstName       string `json:"FirstName"`
+	LastName        string `json:"LastName"`
+	EmailAddress    string `json:"EmailAddress"`
+	IncludeInEmails bool   `json:"IncludeInEmails"`
 }
