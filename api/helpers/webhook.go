49771eb87832a211f7e5d0b66f82636ca08a47b4
Jaime Boardman <jaime@karman.digital>
2024-08-28 15:52:54 +0100

    update webhook validation

--- api/helpers/webhook.go
+++ api/helpers/webhook.go
@@ -1,38 +1,24 @@
 package helpers
 
 import (
-	"bytes"
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/base64"
-	"encoding/json"
 	"hash"
 
 	xeroerrors "github.com/karman-digital/xero-go/app/errors"
 )
 
 func ValidateWebhookSignature(method, signature string, secret, body []byte) error {
-	checkSum, err := toCompactJSONString(body)
-	if err != nil {
-		return err
-	}
-	hash := encrypt(secret, checkSum)
+	hash := encrypt(secret, body)
 	if signature != base64.StdEncoding.EncodeToString(hash.Sum(nil)) {
 		return xeroerrors.ErrInvalidSignature
 	}
 	return nil
 }
 
-func encrypt(secret []byte, input string) hash.Hash {
+func encrypt(secret []byte, input []byte) hash.Hash {
 	hash := hmac.New(sha256.New, secret)
-	hash.Write([]byte(input))
+	hash.Write(input)
 	return hash
-}
-
-func toCompactJSONString(input []byte) (string, error) {
-	buffer := new(bytes.Buffer)
-	if err := json.Compact(buffer, input); err != nil {
-		return "", err
-	}
-	return buffer.String(), nil
 }
