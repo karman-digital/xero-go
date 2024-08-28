33a7d9b996c3458210ae7967b41d72e7edf06739
Jaime Boardman <jaime@karman.digital>
2024-08-28 16:06:43 +0100

    update webhook validation

--- api/helpers/webhook.go
+++ api/helpers/webhook.go
@@ -4,7 +4,6 @@ import (
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/base64"
-	"fmt"
 	"hash"
 
 	xeroerrors "github.com/karman-digital/xero-go/app/errors"
@@ -12,8 +11,6 @@ import (
 
 func ValidateWebhookSignature(method, signature string, secret, body []byte) error {
 	hash := encrypt(secret, body)
-	fmt.Println("hash: ", hash)
-	fmt.Println("signature: ", signature)
 	if signature != base64.StdEncoding.EncodeToString(hash.Sum(nil)) {
 		return xeroerrors.ErrInvalidSignature
 	}
