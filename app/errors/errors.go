80b25831778cba37eb3a46ad6c1945cb9af708f4
Jaime Boardman <jaime@karman.digital>
2024-08-28 15:16:10 +0100

    add webhook validation

--- app/errors/errors.go
+++ app/errors/errors.go
@@ -9,3 +9,5 @@ var ErrNotFound = errors.New("not found")
 var ErrInternal = errors.New("internal error, unrelated to xero")
 
 var ErrApiError = errors.New("xero api error")
+
+var ErrInvalidSignature = errors.New("invalid signature")
